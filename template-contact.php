b9a8998fec7ed8c09b0e78fed33c44b6d54b3a68
fawaz mohammed <alkhdrfwaz11@gmail.com>
2025-11-05 19:53:08 +0300

    a

--- template-contact.php
+++ template-contact.php
@@ -5,4 +5,28 @@
 get_header(); 
 ?>
 
+<?php
+while ( have_posts() ) :
+	the_post();
+?>
+
+<div class="container fw-page pt-4">
+    <div class="row">
+        <div class="col-12 col-lg-9">
+            <?php the_title( '<h1 class="text-sub fw-bold">', '</h1>' ); ?>
+            <hr class="text-third">
+            <div class="fw-page-content shadow-sm rounded-3 bg-white p-3 p-lg-4">
+                <?php the_content(); ?>
+            </div>
+        </div>
+        <div class="col-12 col-lg-3">
+            <div class="fw-sidebar mt-lg-5 mt-0">
+                <iframe data-form-title="form" height="450" style="border: none; width: 100%" src='https://staging.app.atiraspace.com/api/form/68ed2678f5c05bd39291e8e3/generate'></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
