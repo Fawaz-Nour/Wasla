83d6caa45f0324793004c930f603d868bbfad858
fawaz mohammed <alkhdrfwaz11@gmail.com>
2025-11-09 22:25:15 +0300

    header-hero

--- template-parts/header/fw-header.php
+++ template-parts/header/fw-header.php
@@ -1,7 +1,36 @@
+<?php
+    // Theme Customizer variables (logo, email, phone, social links)
+    $logo      = esc_url( get_theme_mod('fw_logo') );
+    $email     = esc_html( trim( get_theme_mod('fw_email') ) );
+    $phone     = esc_html( trim( get_theme_mod('fw_phone') ) );
+	$whatsapp_floating   = esc_url( trim( get_theme_mod('fw_floating_whatsapp') ) );
+?>
 <!-- Header -->
+<div class="top-header bg-main py-1 d-none d-lg-block">
+	<div class="container">
+		<div class="row justify-content-between align-items-center flex-wrap">
+			<div class="col-6">
+				<div class="d-flex align-items-center gap-3">
+					<a href="mailto:<?php echo esc_attr($email); ?>" class="text-white">
+						<i class="fa-solid fa-envelope me-1"></i> <?php echo esc_attr($email); ?>
+					</a>
+					<a href="https://wa.me/<?php echo preg_replace('/\D/', '', $whatsapp_floating); ?>" class="text-white">
+						<i class="fa-solid fa-phone me-1"></i> +<?php echo preg_replace('/\D/', '', $whatsapp_floating); ?>
+					</a>
+				</div>
+			</div>
+			<div class="col-6">
+				<div class="text-end">
+					<p class="m-0 text-white"><i class="fa-solid fa-bolt me-1"></i><?php _e('ابدأ السنة بقوة! خصم 45% على جميع الدورات لفترة محدودة.', 'wasla') ?></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <header id="fw-header" class="fw-header">
 	<div class="container">
-		<div class="row align-items-center py-3">
+		<div class="row align-items-center py-1">
 			
 			<div class="col-6 col-lg-2">
 				<div class="fw-logo">
@@ -34,9 +63,15 @@
 				</div>
 
 				<div class="fw-header-buttons d-none d-lg-block">
-					<a class="btn fw-btn btn-sub" href="<?php echo esc_url( wp_registration_url() ); ?>">
-						<i class="fa-brands fa-whatsapp me-1 fs-5"></i> <?php _e('استفسر الآن', 'wasla'); ?>
-					</a>
+					<?php if ( $whatsapp_floating ) : ?>
+						<a class="btn fw-btn btn-sub" href="https://wa.me/<?php echo preg_replace('/\D/', '', $whatsapp_floating); ?>">
+							<i class="fa-brands fa-whatsapp me-1 fs-5"></i> <?php _e('استفسر الآن', 'wasla'); ?>
+						</a>
+					<?php else : ?>
+						<a class="btn fw-btn btn-sub" href="https://wa.me/">
+							<i class="fa-brands fa-whatsapp me-1 fs-5"></i> <?php _e('استفسر الآن', 'wasla'); ?>
+						</a>
+					<?php endif; ?>
 				</div>
 
 				<div class="d-lg-none">
@@ -71,9 +106,15 @@
             ) );
             ?>
 			<hr>
-			<a class="btn fw-btn btn-sub" href="<?php echo esc_url( wp_registration_url() ); ?>">
-				<i class="fa-brands fa-whatsapp me-1 fs-5"></i> <?php _e('استفسر الآن', 'wasla'); ?>
-			</a>
+			<?php if ( $whatsapp_floating ) : ?>
+				<a class="btn fw-btn btn-sub" href="https://wa.me/<?php echo preg_replace('/\D/', '', $whatsapp_floating); ?>">
+					<i class="fa-brands fa-whatsapp me-1 fs-5"></i> <?php _e('استفسر الآن', 'wasla'); ?>
+				</a>
+			<?php else : ?>
+				<a class="btn fw-btn btn-sub" href="https://wa.me/">
+					<i class="fa-brands fa-whatsapp me-1 fs-5"></i> <?php _e('استفسر الآن', 'wasla'); ?>
+				</a>
+			<?php endif; ?>
 		</div>
 	</div>
 </header>
--- template-parts/home/fw-hero.php
+++ template-parts/home/fw-hero.php
@@ -1,21 +1,27 @@
+<?php
+	$whatsapp_floating   = esc_url( trim( get_theme_mod('fw_floating_whatsapp') ) );
+?>
+
 <!-- Hero -->
 <section class="fw-hero" id="fw-home">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-12 col-md-6" data-aos="fade-right" data-aos-duration="1000">
                 <div class="hero-content">
-                    <h1>اربط <span class="highlight">مهاراتك</span> بمن يحتاجها</h1>
-                    <p>منصة عربية بسيطة تجمع بين مقدمي الخدمات والعملاء. ابدأ في بيع خدماتك أو اعثر على المحترف المناسب لمشروعك</p>
+                    <h1>
+                        <?php _e('كل مهارة متوفرة <span class="highlight">هنا</span>', 'wasla'); ?>
+                    </h1>
+                    <p><?php _e('تعلم بسهولة وارتقِ بمهاراتك خطوة خطوة — دورات وخدمات متوفرة لك في مكان واحد لتبدأ وتتقدم بالطريقة التي تناسبك.', 'wasla'); ?></p>
                     <div class="hero-buttons" data-aos="fade-up" data-aos-delay="300">
-                        <a href="<?php echo esc_url( wp_registration_url() ); ?>" class="btn fw-btn btn-main">ابدأ كبائع</a>
-                        <a href="<?php echo esc_url( wp_registration_url() ); ?>" class="btn fw-btn btn-sub">تصفح الخدمات</a>
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>?cat=22" class="btn fw-btn btn-main"><?php _e('تصفح الدورات', 'wasla')?></a>
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>?cat=19" class="btn fw-btn btn-sub"><?php _e('اطلب خدمة', 'wasla')?></a>
                     </div>
                 </div>
             </div>
             <div class="col-12 col-md-6" data-aos="fade-right" data-aos-duration="1200" data-aos-delay="500">
                 <div class="hero-image d-none d-lg-block">
                     <div class="hero-image-container">
-                        <img class="shadow rounded-5" src="http://wasla.local/wp-content/uploads/2025/10/hero.jpg" alt="Hero">
+                        <img class="shadow rounded-5" src="http://wasla.local/wp-content/uploads/2025/11/person-working-laptop-with-cup-coffee-table-scaled.jpg" alt="Hero">
                     </div>
                 </div>
             </div>
